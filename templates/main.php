55f806fa1a554bda126cde968c77ec518097ef38
Loic Blot <loic.blot@unix-experience.fr>
2015-07-18 08:14:05 +0000

    Prepare setting currentCity home template

--- templates/main.php
+++ templates/main.php
@@ -34,7 +34,8 @@
 		<div id="city-weather-panel">
 			<div class="city-name">
 				{{ currentCity.name }}, {{ currentCity.sys.country }}
-				<img class="home-icon" src="{{ owncloudAppImgPath }}/img/home-{{ selectedCityId == homeCity ? '' : 'no' }}pick.png" />
+				<img ng-show="selectedCityId == homeCity" src="{{ owncloudAppImgPath }}/img/home-pick.png" />
+				<img class="home-icon" ng-click="setHome(selectedCityId);" ng-show="selectedCityId != homeCity" src="{{ owncloudAppImgPath }}/img/home-nopick.png" />
 			</div>
 			<div class="city-current-temp">{{ currentCity.main.temp }}°C</div>
 			<div class="city-current-pressure">Pressure: {{ currentCity.main.pressure }} hpa</div>
