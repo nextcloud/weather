6afe8d16e0181f3621f205a944c3f6c86f0ab4ac
Loic Blot <loic.blot@unix-experience.fr>
2017-03-01 16:48:24 +0100

    Update admin template

--- templates/admin.php
+++ templates/admin.php
@@ -1,5 +1,10 @@
-<form id="weather">
-	<div class="section">
-		<h2><?php p($l->t('Weather')) ?></h2>
-	</div>
-</form>
+<div id="weather" class="section">
+	<h2><?php p($l->t('Weather')) ?></h2>
+	<p>
+		<label>
+			<span><?php p($l->t('OpenWeatherMap API Key')) ?></span>
+			<input id="openweathermap-api-key" type="text" value="<?php p($_['openweathermap_api_key']) ?>" />
+		</label>
+		<input type="submit" id="submitOWMKey" value="<?php p($l->t('Save')); ?>"/>
+	</p>
+</div>
