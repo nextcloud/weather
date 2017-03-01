fd8d1b257629dc9cd082193006019718f588912c
Loic Blot <loic.blot@unix-experience.fr>
2017-03-01 17:20:21 +0100

    Better admin template

--- templates/admin.php
+++ templates/admin.php
@@ -1,10 +1,10 @@
 <div id="weather" class="section">
 	<h2><?php p($l->t('Weather')) ?></h2>
 	<p>
-		<label>
-			<span><?php p($l->t('OpenWeatherMap API Key')) ?></span>
-			<input id="openweathermap-api-key" type="text" value="<?php p($_['openweathermap_api_key']) ?>" />
-		</label>
+		<label for="openweathermap-api-key"><?php p($l->t('OpenWeatherMap API Key')) ?></label>
+		<br />
+		<input id="openweathermap-api-key" type="text" value="<?php p($_['openweathermap_api_key']) ?>" />
 		<input type="submit" id="submitOWMKey" value="<?php p($l->t('Save')); ?>"/>
 	</p>
 </div>
+
