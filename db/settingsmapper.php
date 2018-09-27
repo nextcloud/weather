7b7ba926a4e255f4b00d20e4b491ee704b6bf73e
e-alfred <e-alfred@users.noreply.github.com>
2018-09-27 18:24:41 +0200

    Fix deprecated OCP\DB calls for NC14+

--- db/settingsmapper.php
+++ db/settingsmapper.php
@@ -7,7 +7,7 @@
  *
  * @author Loic Blot <loic.blot@unix-experience.fr>
  * @copyright Loic Blot 2015
- * @copyright Loic Blot 2018
+ * @copyright e-alfred 2018 
  */
 
 namespace OCA\Weather\Db;
@@ -16,58 +16,44 @@ use \OCP\IDBConnection;
 
 use \OCP\AppFramework\Db\Mapper;
 
-class CityMapper extends Mapper {
+class SettingsMapper extends Mapper {
         public function __construct (IDBConnection $db) {
-                parent::__construct($db, 'weather_city');
+                parent::__construct($db, 'weather_config');
         }
 
-        public function load ($id) {
-                $sql = "SELECT id, name, user_id FROM *PREFIX*weather_city WHERE `id` ='" . $id . "'";
-                $result = $this->db->executequery($sql);
-
-                if ($row = $result->fetch()) {
-                        return $row;
-                }
-                return null;
+        public function setHome ($userId, $cityId) {
+                $this->setSetting("home", $userId, $cityId);
         }
 
-        public function count() {
-                $sql = "SELECT count(*) AS ct FROM *PREFIX*weather_city WHERE `user_id` ='" . $userId . "'";
-                $result = $this->db->executequery($sql);
+        public function getHome ($userId) {
+                return $this->getSetting($userId, "home");
+        }
+
+        public function setMetric ($userId, $metric) {
+                $this->setSetting("metric", $userId, $metric);
+        }
+
+        public function getMetric ($userId) {
+                return $this->getSetting($userId, "metric");
+        }
+
+        public function setSetting ($settingName, $userId, $settingValue) {
+                $sql = "DELETE FROM *PREFIX*weather_config  WHERE `user` = '" . $userId . "' and `key` = '" . $settingName . "'";
+                $this->db->executequery($sql);
+
+                $sql = "INSERT INTO *PREFIX*weather_config (`user`,`key`,`value`) VALUES ('" . $userId . "','" . $settingName . "','" . $settingValue . "')";
+                $this->db->executequery($sql);
+        }
+
+        public function getSetting ($userId, $settingName) {
+                $sql = "SELECT value FROM *PREFIX*weather_config WHERE `user` ='" . $userId . "' and `key` ='" . $settingName . "'";
+                $result = $this->db->executeQuery($sql);
+
                 if ($row = $result->fetch()) {
-                        return $row['ct'];
+                        return $row["value"];
                 }
                 return 0;
         }
 
-        public function exists ($id) {
-                return ($this->load($id));
-        }
-
-        public function getAll ($userId) {
-                $sql = "SELECT id, name FROM *PREFIX*weather_city WHERE `user_id` = '" . $userId . "'";
-                $result = $this->db->executequery($sql);
-
-                $cities = array();
-                while ($row = $result->fetch()) {
-                        $cities[] = $row;
-                }
-                return $cities;
-        }
-
-        public function create ($userId, $name) {
-                $this->db->beginTransaction();
-                $sql = "INSERT INTO *PREFIX*weather_city(`user_id`, `name`) VALUES ('" . $userId . "','" . $name . "')";
-                $this->db->executequery($sql);
-                $this->db->commit();
-
-                $sql = "SELECT max(id) as maxid FROM *PREFIX*weather_city WHERE `user_id` = '" . $userId . "' and `name` = '" . $name . "'";
-                $result = $this->db->executequery($sql);
-
-                if ($row = $result->fetch()) {
-                        return $row['maxid'];
-                }
-                return null;
-        }
 };
 ?>
