612192a6f1917194eeef0202017633dd7ab8d55f
Balint Erdosi <erdosib@gmail.com>
2019-08-08 01:28:23 +0200

    added license
    Signed-off-by: Balint Erdosi <erdosib@gmail.com>

--- js/widget.js
+++ js/widget.js
@@ -1,3 +1,23 @@
+/**
+ *
+ * @copyright Copyright (c) 2019, Balint Erdosi (erdosib@gmail.com)
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 /** global: OCA */
 /** global: net */
@@ -43,8 +63,6 @@ console.log("OCA", OCA, "net", net)
 		divWeather.querySelector(".humidityValue").innerHTML = result.value.humidity;
 		divWeather.querySelector(".windValue").innerHTML = result.value.wind;
 	}
-
-	// TODO rewrite the above with JQuery
 
 
 	OCA.DashBoard.Weather = Weather;
